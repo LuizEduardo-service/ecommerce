2013a7ff29d9931fa4929b69235f90369f08709e
LuizEduardo-service <luizeduardo.service@outlook.com>
2024-02-27 08:15:41 -0300

    atualização

--- produto/models.py
+++ produto/models.py
@@ -2,22 +2,14 @@ from django.db import models
 from PIL import Image
 import os
 from django.conf import settings
-# Create your models here.
-
-"""    
-    Variacao:
-        nome - char
-        produto - FK Produto
-        preco - Float
-        preco_promocional - Float
-        estoque - Int"""
+from django.utils.text import slugify
 
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
     descricao_curta = models.TextField(max_length=255)
     descricao_longa = models.TextField()
     imagem = models.ImageField(upload_to='produto_imagens/%Y/%m', blank=True, null=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     preco_marketing = models.FloatField()
     preco_marketing_promocional = models.FloatField(default=0)
     tipo = models.CharField(default='V', max_length=1, choices=(('V', 'Variação'), ('S', 'Simples')))
@@ -25,6 +17,14 @@ class Produto(models.Model):
     class Meta:
         verbose_name = 'Produto'
         verbose_name_plural = 'Produtos'
+
+    def get_preco_formatado(self):
+        return f'R$ {self.preco_marketing:.2f}'.replace('.', ',')
+    get_preco_formatado.short_description = 'Preço'
+
+    def get_preco_formatado_promo(self):
+        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', ',')
+    get_preco_formatado_promo.short_description = 'Preço Promo'
 
     @staticmethod
     def resize_image(img, new_width=800):
@@ -34,10 +34,10 @@ class Produto(models.Model):
         new_height = round(new_width * original_height) / original_width
 
 
-        if original_width <= new_width:
-            img_pil.close()
+        # if original_width <= new_width:
+        #     img_pil.close()
 
-        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img = img_pil.resize((new_width, int(new_height)), Image.LANCZOS)
         new_img.save(
             img_full_path,
             optimize=True,
@@ -47,6 +47,11 @@ class Produto(models.Model):
         print(img.name)
 
     def save(self, *args, **kwargs):
+
+        if not self.slug:
+            slug = f'{slugify(self.nome)}-{self.pk}'
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
         image_size = 800
--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from . import models
+
 
 # Create your views here.
+
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    
+class DetalheProduto(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('Detalhe Produto')
+
+class AdicionarCarrinho(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('Adicionar Carrinho')
+
+class RemoverCarrinho(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('Remover Carrinho')
+
+class Carrinho(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('Carrinho')
+
+class Finalizar(View):
+    def get(self,*args, **kwargs):
+        return HttpResponse('Finalizar')
